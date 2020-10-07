9f1a6a5292107fe4f5f4df75095ca777dbb805ec
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-08 00:34:56 +0500

    Carousel with Admin Panel

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router} from 'react-router-dom';
+// import { BrowserRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+ 
     
   <React.StrictMode>
     <App />
-  </React.StrictMode> 
-  </Router>,
+  </React.StrictMode> ,
+
   document.getElementById('root')
 );
 
