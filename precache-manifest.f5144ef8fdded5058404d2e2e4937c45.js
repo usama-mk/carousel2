543d230b317d59ec3bcef43d29f400bbdb8e6c86
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-07 23:44:21 +0500

    Updates

---
+++ precache-manifest.f5144ef8fdded5058404d2e2e4937c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b46aa9a18c95fecdec142849dcff689",
+    "url": "/carousel2/index.html"
+  },
+  {
+    "revision": "bf6fcf7e8addfad95e7c",
+    "url": "/carousel2/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4bb80823c7bc0644ff09",
+    "url": "/carousel2/static/css/main.0e421966.chunk.css"
+  },
+  {
+    "revision": "bf6fcf7e8addfad95e7c",
+    "url": "/carousel2/static/js/2.fae37b36.chunk.js"
+  },
+  {
+    "revision": "999b2829de03f03f5ebf70720cbcd78e",
+    "url": "/carousel2/static/js/2.fae37b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb80823c7bc0644ff09",
+    "url": "/carousel2/static/js/main.f073255b.chunk.js"
+  },
+  {
+    "revision": "93c1a7b390681b8d20de",
+    "url": "/carousel2/static/js/runtime-main.c1522d29.js"
+  }
+]);
