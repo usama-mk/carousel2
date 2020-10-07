7a1dbf4e43a6a45c81e782568865964168cd8870
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-07 23:20:44 +0500

    Carousel with Admin Panel

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import DemoCarousel from './Pages/carousel';
+import Login from './Pages/Login';
+
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <Switch>
+      <Route exact path='/carousel2' render={()=>(<DemoCarousel adminCheckP={false} />)}  />
+     <Route exact path='/admin' component={Login}  />
+     <Route exact path='/admin/carousel2' component={DemoCarousel}/>
+    
+     </Switch>
+    
+    </div>
+  );
+}
+
+export default App;
